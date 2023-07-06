c5e084b649a2a2f0f21f102f512be2f477c2a30d
Rizzzi Git <rizzzigit@gmail.com>
2023-07-07 00:56:26 +0800

    sync segment size and message size

--- RBMP/src/WebSocketStreamBridge.cs
+++ RBMP/src/WebSocketStreamBridge.cs
@@ -7,6 +7,10 @@ internal class WebSocketStreamBridge : Stream
   public WebSocketStreamBridge(WebSocket websocket)
   {
     WebSocket = websocket;
+
+    MessageSegmentLength = ConnectionConfig.ConfigBufferSize;
+    MessageSegmentPartialBuffer = new byte[4];
+    MessageSegmentPartiaBufferPosition = 0;
   }
 
   private WebSocket WebSocket;
@@ -31,6 +35,8 @@ internal class WebSocketStreamBridge : Stream
 
     try
     {
+      try { _Write(new byte[0], 0, 0, true); }
+      catch { }
       WebSocket.CloseAsync(WebSocketCloseStatus.NormalClosure, null, new(false)).Wait();
     }
     catch (AggregateException exception)
@@ -57,7 +63,7 @@ internal class WebSocketStreamBridge : Stream
 
       if (task.Result.CloseStatus != null)
       {
-        try { Close(); } catch {}
+        try { Close(); } catch { }
       }
 
       return task.Result.Count;
@@ -68,9 +74,9 @@ internal class WebSocketStreamBridge : Stream
     }
   }
 
-  public override void Write(byte[] buffer, int offset, int count)
+  private void _Write(byte[] buffer, int offset, int count, bool flush)
   {
-    var task = WebSocket.SendAsync(new(buffer, offset, count), WebSocketMessageType.Binary, true, new(false));
+    var task = WebSocket.SendAsync(new(buffer, offset, count), WebSocketMessageType.Binary, flush, new(false));
 
     try
     {
@@ -79,6 +85,52 @@ internal class WebSocketStreamBridge : Stream
     catch (AggregateException exception)
     {
       throw exception.GetBaseException();
+    }
+  }
+
+  private int MessageSegmentLength;
+  private byte[] MessageSegmentPartialBuffer;
+  private int MessageSegmentPartiaBufferPosition;
+  private int MessageSegmentPartialBufferRemaining => 4 - MessageSegmentPartiaBufferPosition;
+  public override void Write(byte[] buffer, int offset, int count)
+  {
+    if (count == 0)
+    {
+      return;
+    }
+
+    if (MessageSegmentLength == 0)
+    {
+      int index = 0;
+      for (; (MessageSegmentPartialBufferRemaining > 0) && (index < count); index++)
+      {
+        MessageSegmentPartialBuffer[MessageSegmentPartiaBufferPosition] = buffer[index + offset];
+        MessageSegmentPartiaBufferPosition++;
+      }
+
+      if (MessageSegmentPartialBufferRemaining == 0)
+      {
+        MessageSegmentPartiaBufferPosition = 0;
+        MessageSegmentLength = index + BitConverter.ToInt32(MessageSegmentPartialBuffer);
+      }
+    }
+
+    if ((MessageSegmentLength == 0 || MessageSegmentLength > count))
+    {
+      _Write(buffer, offset, count, false);
+
+      if (MessageSegmentLength != 0)
+      {
+        MessageSegmentLength -= count;
+      }
+    }
+    else
+    {
+      _Write(buffer, offset, MessageSegmentLength, true);
+      var messageSegmentLength = MessageSegmentLength;
+      MessageSegmentLength = 0;
+
+      Write(buffer, offset + messageSegmentLength, count - messageSegmentLength);
     }
   }
 }
